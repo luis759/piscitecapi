539be7885734e10893dc437ac51a97ae9814aafe
Luis759 <luisprador759@gmail.com>
2024-02-28 22:07:14 -0400

    Agregado

--- controlador/controladorversionapp.php
+++ controlador/controladorversionapp.php
@@ -2,7 +2,7 @@
 //Llamada al modelo del especies
 require_once("modelo/versionapp.php");
 $versionapp=new versionapp(); 
-$versionActual=17;
+$versionActual=18;
 //Validacion para uso de algunas funciones especificas
 if($opcion=='getversion'){
     $infoData=$versionapp->getVersion($versionActual);
